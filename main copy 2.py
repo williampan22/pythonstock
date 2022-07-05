0dec5e2500976be6cafde3e9b0c3c668e9ad0ddf
williampan22 <williamlpan22@gmail.com>
2022-07-05 11:46:35 -0700

    multiple tickers

--- main copy 2.py
+++ main copy 2.py
@@ -11,12 +11,14 @@ from pandas_datareader._utils import RemoteDataError
 valid_ticker = False
 valid_investment = False
 valid_date = False
+num_stocks = 0
 
 while valid_ticker == False: 
     try: 
-        #str_arr = input('Enter stock tickers: ').split(',') 
-        #arr = [num.strip().upper() for num in str_arr]
-        stock_ticker = input('Enter a Stock Ticker (eg. AAPL): ').upper()
+        str_arr = input('Enter stock tickers: ').split(',') 
+        stock_ticker = [num.strip().upper() for num in str_arr]
+        
+        
         test = web.DataReader(stock_ticker, 'yahoo', dt.datetime(2022, 1, 1), dt.datetime.today())
         break
     except RemoteDataError: 
@@ -56,48 +58,53 @@ df = web.DataReader( stock_ticker, 'yahoo', start, end)
 
 adj_closings = df['Adj Close']
 
-
-current_price = adj_closings.iloc[-1]
-first_price = adj_closings.iloc[0]
-pricediff = current_price - first_price
-
-current_price_rounded = round(current_price, 2)
-first_price_rounded = round(first_price, 2)
-pricediff_rounded = round(pricediff, 2)
-
-shares = float(initial_investment) / first_price
-shares_rounded = round(shares, 2)
-
-profit = shares * pricediff
-profit_rounded = round(profit, 2)
-
-roi = profit / float(initial_investment) * 100
-roi_rounded = round(roi, 2)
-
-money_now = float(initial_investment) + profit
-money_now_rounded = round(money_now, 2)
-
-print('---------------------------------------------------------')
-print("If you bought $" + initial_investment + " of " + stock_ticker + " stock on " + start_string + ", you would have been able to buy " + str(shares_rounded) + " shares.")
-print(stock_ticker + " closed at $" + str(first_price_rounded) + " on " + start_string + " and closed at $" + str(current_price_rounded) + " today (" + end_string + "). The price difference is " + str(pricediff_rounded))
-print("The profit/loss from buying " + stock_ticker + " on " + start_string + " with an initial investment of $" + initial_investment + " would be $" + str(profit_rounded))
-print("This is a RETURN ON INVESTMENT (ROI) OF " + str(roi_rounded) + "%! You would now have $" + str(money_now_rounded) + ' compared to the inital investment of $' + initial_investment+ '!')
-print("NOTE: these calculations account for stock splits!")
-print('---------------------------------------------------------')
+print(adj_closings.head())
 
 
-adj_closings_list = adj_closings.tolist()
+print(adj_closings[stock_ticker[0]].iloc[0])
 
-adj_closings_graph = df['Adj Close']
-adj_closings_graph.plot()
-plt.title(stock_ticker + " Closing Price vs Time")
-plt.xlabel('Date')
-plt.ylabel('Adjusted Closing Price')
+for i in range(len(stock_ticker)): 
+    current_ticker = stock_ticker[i]
+    current_price = adj_closings[stock_ticker[i]].iloc[-1]
+    first_price = adj_closings[stock_ticker[i]].iloc[0]
+    pricediff = current_price - first_price
 
+    current_price_rounded = round(current_price, 2)
+    first_price_rounded = round(first_price, 2)
+    pricediff_rounded = round(pricediff, 2)
 
+    shares = float(initial_investment) / first_price
+    shares_rounded = round(shares, 2)
 
+    profit = shares * pricediff
+    profit_rounded = round(profit, 2)
 
-plt.show()
+    roi = profit / float(initial_investment) * 100
+    roi_rounded = round(roi, 2)
 
+    money_now = float(initial_investment) + profit
+    money_now_rounded = round(money_now, 2)
+
+    print('---------------------------------------------------------')
+    print("If you bought $" + initial_investment + " of " + current_ticker + " stock on " + start_string + ", you would have been able to buy " + str(shares_rounded) + " shares for an average price of " + str(first_price_rounded) +  ".")
+    print("The return from buying " + current_ticker + " on " + start_string + " with an initial investment of $" + initial_investment + " would be $" + str(profit_rounded) + " for a ROI of " + str(roi_rounded) + "%!. You would now have $" + str(money_now_rounded) + ".")
     
+print('---------------------------------------------------------')
+print("NOTE: these calculations account for stock splits!")
+
+# adj_closings_list = adj_closings.tolist()
+
+# adj_closings_graph = df['Adj Close']
+# adj_closings_graph.plot()
+# plt.title(stock_ticker + " Closing Price vs Time")
+# plt.xlabel('Date')
+# plt.ylabel('Adjusted Closing Price')
+
+
+
+
+# plt.show()
+
+# print(df.head())
+
 
